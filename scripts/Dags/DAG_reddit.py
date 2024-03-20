70017633256698ba304fcd41874c78acc40c21b8
Shuyan Huang <shuyanhuang2014@gmail.com>
2024-03-19 23:02:16 -0400

    add another workflow to allow mysql gets update after redis2mongodb

--- scripts/Dags/DAG_reddit.py
+++ scripts/Dags/DAG_reddit.py
@@ -2,7 +2,8 @@ import datetime
 import pendulum
 import sys
 sys.path.append("/home/cissy/repos/RedditThread_ETL/scripts")
-from Reddit_Scraping import run_update
+from Reddit_Scraping import redis_mongodb_update
+from Sentiment_Processing import update_mysql
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
@@ -16,9 +17,15 @@ with DAG(
     dagrun_timeout=datetime.timedelta(minutes=5),
     tags=["reddit_pipeline"],
 ) as dag:
-        run_my_script = PythonOperator(
+        Redis2MongoDB = PythonOperator(
         task_id='Load_reddit_posts_to_mongoDB',
-        python_callable=run_update,
+        python_callable=redis_mongodb_update,
+    )
+        Update_MySQL = PythonOperator(
+        task_id='Update_Sentiment_Analysis_to_MySQL',
+        python_callable=update_mysql,
     )
 
-run_my_script
+        
+
+Redis2MongoDB >> Update_MySQL
